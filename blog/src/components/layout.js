2e5c54f9521032480b67e84f9b7a917854a2610e
TimothyHuynh212 <skyborn225@gmail.com>
2022-03-11 04:42:52 -0600

    Converted inline styles to Styled Components

--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -8,9 +8,17 @@
 import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import styled from "styled-components"
 
 import Header from "./header"
 import "./layout.css"
+
+const Content = styled.div`
+  margin: 0 auto;
+  max-width: 960px;
+  padding: 0 1.0875rem 1.45rem;
+  padding-top: 0;
+  `
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -25,14 +33,8 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata?.title || `Title`} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <Header siteTitle={data.site.siteMetadata.title || `Title`} />
+      <Content>
         <main>{children}</main>
         <footer
           style={{
@@ -43,7 +45,7 @@ const Layout = ({ children }) => {
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
         </footer>
-      </div>
+      </Content>
     </>
   )
 }
